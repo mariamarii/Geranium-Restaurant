d03e4e0cfc233ba324fb6522f7695a2aa7d36b72
maryam <mm5252@fayoum.edu.eg>
2024-04-04 14:03:38 +0200

    .

---
+++ project/gallery/gallery.java
@@ -0,0 +1,62 @@
+ <script async src="https://www.googletagmanager.com/gtag/js?id=UA-9357165-79"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag() {
+                dataLayer.push(arguments);
+            }
+            gtag('js', new Date());
+
+            gtag('config', 'UA-9357165-79');
+        </script>        <div id="desktop-delimiter"></div>
+        <div id="overlay">
+                    </div>
+                    <style>
+
+            @media all and (max-width: 1024px)
+            {
+                html {
+                    margin-top: 0px !important;
+                }
+
+                #wpadminbar
+                {
+                    display:none;
+                }
+            }
+
+            #overlay
+            {
+                position: fixed;
+                top: 0px;
+                left: 0px;
+                width: 100%;
+                height: 100%;
+                background: #000;
+                z-index: 1000;
+            }
+
+            #overlay > div
+            {
+                padding: 15px;
+
+                position: absolute;
+                top: 50%;
+                left: 50%;
+                transform: translate(-50%,-50%);
+            }
+
+            #overlay img
+            {
+                width: 100%;
+                max-width: 300px;
+                height: auto;
+            }
+
+        </style>
+         <style>
+        #the-hero
+        {
+            background: url('https://adachirestaurant.com/wp-content/uploads/2020/11/gallery-hero-scaled.jpg');
+            background-size: cover;
+        }
+    </style>
